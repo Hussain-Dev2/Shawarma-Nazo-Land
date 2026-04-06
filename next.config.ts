53277787d7d55bd3b4fcb9bc35f0cb4c77d6d12a
Hussain-Dev <dev.hussain.iq@gmail.com>
2026-04-06 20:35:11 +0300

    nase

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'plus.unsplash.com' },
       { protocol: 'https', hostname: 'www.transparenttextures.com' },
-      { protocol: 'https', hostname: 'lpwtscydxjswhmkqtmqh.supabase.co' },
+      { protocol: 'https', hostname: 'sefqikeblpugukodcron.supabase.co' },
     ],
   },
 };
